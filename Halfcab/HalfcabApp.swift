61ff676408911ebe0045e932bd6d7d1cb65405b4
rihardslozins <rihards.lozins@gmail.com>
2022-04-21 10:37:15 +0300

    Initial Commit

---
+++ Halfcab/HalfcabApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfcabApp.swift
+//  Halfcab
+//
+//  Created by Rihards Lozins on 21/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HalfcabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
