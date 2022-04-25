61e36e1fdcf2c5d48542a27f428672f26b72ac28
rihardslozins <rihards.lozins@gmail.com>
2022-04-25 21:27:21 +0300

    Portrait mode fix.

---
+++ Halfcab/Utility/Constant.swift
@@ -0,0 +1,16 @@
+//
+//  Constant.swift
+//  Halfcab
+//
+//  Created by Rihards Lozins on 21/04/2022.
+//
+
+import SwiftUI
+
+//Headers
+let headers: [Header] = Bundle.main.decode("headers.json")
+
+//Colors
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color("ColorGray")
+let colorRed: Color = Color("ColorRed")
