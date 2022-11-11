6cf3e97f087aa5842e9cdafe9c7b04961eff7dbd
rihardslozins <rihards.lozins@gmail.com>
2022-11-11 17:53:51 +0200

    Menu View added to the project.

--- Halfcab/View/Home/MenuView.swift
+++ Halfcab/View/Home/MenuView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct MenuView: View {
+    // MARK: - Properties
+    @State var showMenu: Bool = false
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: {}, label: {
+                HStack(spacing: 15) {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    
+                    Text("Cart")
+                        .font(Font.custom("ProximaNova-Bold", size: 20))
+                        .foregroundColor(.black)
+                    
+                    Spacer(minLength: 0)
+                }
+                .padding()
+                
+            })
+            Spacer()
+            
+            HStack {
+                Spacer()
+                
+                Text("Version 0.1")
+                    .font(Font.custom("ProximaNova-Bold", size: 16))
+                
+            } // End of HStack
+            .padding(10)
+            
+        } // End of VStack
+        .frame(width: UIScreen.main.bounds.width / 1.6)
+        .background(Color.white.ignoresSafeArea())
     }
 }
 
+// MARK: - Preview
 struct MenuView_Previews: PreviewProvider {
     static var previews: some View {
         MenuView()
